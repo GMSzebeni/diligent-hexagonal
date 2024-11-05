f088e1e696dd07bb9626cc155f0e71cabb212830
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-05 15:12:07 +0100

    add adapters for task 7

--- src/primary-adapters/prompt-input-primary-adapter.ts
+++ src/primary-adapters/prompt-input-primary-adapter.ts
@@ -1,5 +1,11 @@
 import createPrompt from 'prompt-sync';
+import { ProvideNamePrimaryPort } from '../business/greeting-service';
 
-export function runPromptApp( ) {
-  
+export function runPromptApp(greetingService: ProvideNamePrimaryPort) {
+    const prompt = createPrompt();
+    
+    const name = prompt("Your name is: ");
+    
+    const greeting = greetingService.greet(name);
+    console.log(greeting);
 }
--- src/secondary-adapters/txt-store-secondary-adapter.ts
+++ src/secondary-adapters/txt-store-secondary-adapter.ts
@@ -1,1 +1,14 @@
-export class TxtStoreSecondaryAdapter {}
+import { StoreGreetingSecondaryPort } from "../business/greeting-service";
+import { FileStore } from "../file-store";
+
+export class TxtStoreSecondaryAdapter implements StoreGreetingSecondaryPort {
+    private readonly store: FileStore<string>;
+
+    constructor(store: FileStore<string>) {
+        this.store = store;
+    }
+    
+    save(name: string) {
+        this.store.write(`Saved: ${name}`);
+    }
+}
