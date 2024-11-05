28d9c895fc522df7ddc15dfec37d08f6e8620c09
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-05 15:12:23 +0100

    add prompts for task 7

---
+++ src/prompt.ts
@@ -0,0 +1,9 @@
+import { GreetingService } from "./business/greeting-service";
+import { FileStore } from "./file-store";
+import { runPromptApp } from "./primary-adapters/prompt-input-primary-adapter";
+import { TxtStoreSecondaryAdapter } from "./secondary-adapters/txt-store-secondary-adapter";
+
+const store = new FileStore('greetings.txt');
+const storeAdapter = new TxtStoreSecondaryAdapter(store);
+const greetingService = new GreetingService(storeAdapter);
+runPromptApp(greetingService);
