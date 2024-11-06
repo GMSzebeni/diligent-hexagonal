487410454f2f0ba1f8d005e872157fc559ebfc86
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-06 14:46:49 +0100

    add primary adapter for task 8

--- src/primary-adapters/fastify-api-primary-adapter.ts
+++ src/primary-adapters/fastify-api-primary-adapter.ts
@@ -1,11 +1,44 @@
 import fastify from 'fastify';
+import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
+import { GreetingService } from '../business/greeting-service';
 
-export default function createApp(options = {}) {
-  const app = fastify(options)
+export default function createApp(options = {}, greetingService: GreetingService) {
+  const app = fastify(options).withTypeProvider<JsonSchemaToTsProvider>();
+
+  const postGreetingSchema = {
+    body: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        }        
+      },
+      required: ['name'],
+      additionalProperties: false
+    }
+  } as const
 
   app.get('/hello', async () => {
     return { hello: 'World!' }
   })
+
+  app.post(
+    '/greet', 
+    { schema: postGreetingSchema },
+    async (request, reply) => {
+      const { name } = request.body;
+
+      if (name === "") {
+        return reply.status(400).send("Please privide a name!");
+      }
+
+      const greeting = greetingService.greet(name);
+
+      const greetingObj = { "greeting": greeting };
+
+      return reply.status(200).send(greetingObj)
+    }
+  )
 
   return app;
 }
