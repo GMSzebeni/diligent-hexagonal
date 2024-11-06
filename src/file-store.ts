19e0a92e0ddbd62acc8c4c502ea75c2586d578a6
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-06 14:40:31 +0100

    update filestore to work with json and add read method to use for task 8

--- src/file-store.ts
+++ src/file-store.ts
@@ -1,15 +1,32 @@
 import { existsSync, PathLike, writeFileSync } from "node:fs";
-import { appendFile } from "node:fs/promises";
+import { appendFile, readFile } from "node:fs/promises";
 
-export class FileStore<T> {
-    constructor(private readonly path: PathLike) {
+type FileFormat = "json" | "txt";
+
+export class FileStore {
+    constructor(private readonly path: PathLike, private readonly format: FileFormat) {
         if(!existsSync(this.path)) {
-            writeFileSync(this.path, '', 'utf-8');
+            if (this.format === 'txt') {
+                writeFileSync(this.path, "", 'utf-8');
+            }
+            if (this.format === 'json') {
+                writeFileSync(this.path, "[]", 'utf-8');
+            }
         }
     }
 
-    async write(data: T) {
-        const content = JSON.stringify(data, null, 2)
-        await appendFile(this.path, content + '\r\n', 'utf-8');
+    async read(): Promise<string[]> {
+        const content = await readFile(this.path, 'utf-8');
+        const data = content ? JSON.parse(content) : [];
+        return data;
+    }
+
+    async write(data: string | string[]) {
+        if (typeof data === 'string') {
+            await appendFile(this.path, data + '\r\n', 'utf-8');
+        } else if (data instanceof Array) {
+            const content = JSON.stringify(data, null, 2)
+            writeFileSync(this.path, content, 'utf-8');
+        }
     }
 }
