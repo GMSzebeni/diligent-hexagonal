566cc6d5f51e2b76a77ba77c5d223246030017ce
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-06 14:49:19 +0100

    update filestore to use txt type because of changes made for task8

--- src/prompt.ts
+++ src/prompt.ts
@@ -3,7 +3,7 @@ import { FileStore } from "./file-store";
 import { runPromptApp } from "./primary-adapters/prompt-input-primary-adapter";
 import { TxtStoreSecondaryAdapter } from "./secondary-adapters/txt-store-secondary-adapter";
 
-const store = new FileStore('greetings.txt');
+const store = new FileStore('greetings.txt', 'txt');
 const storeAdapter = new TxtStoreSecondaryAdapter(store);
 const greetingService = new GreetingService(storeAdapter);
 runPromptApp(greetingService);
