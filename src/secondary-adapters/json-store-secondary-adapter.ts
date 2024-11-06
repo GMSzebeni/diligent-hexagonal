1b2e28cc011b3381272eb4a6096af4c3617e5151
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-06 14:47:16 +0100

    add secondary adapter for task 8

--- src/secondary-adapters/json-store-secondary-adapter.ts
+++ src/secondary-adapters/json-store-secondary-adapter.ts
@@ -1,1 +1,16 @@
-export class JsonStoreSecondaryAdapter {}
+import { StoreGreetingSecondaryPort } from "../business/greeting-service";
+import { FileStore } from "../file-store";
+
+export class JsonStoreSecondaryAdapter implements StoreGreetingSecondaryPort {
+    private readonly store: FileStore;
+
+    constructor(store: FileStore) {
+        this.store = store;
+    }
+
+    async save(greeting: string) {
+        const greetings = await this.store.read() as string[];
+        greetings.push(JSON.parse(`{ "greeting": "${greeting}" }`));
+        await this.store.write(greetings);
+    };
+}
